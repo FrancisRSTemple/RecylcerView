cb846cfcc9e411fc6c5d6da04bab58e3f6a0ff40
Francis R Scallatino <tup31915@temple.edu>
2023-02-16 13:29:41 -0500

    rework ImageAdapter to inflate recyclerview_item.xml into project

--- app/src/main/java/com/example/recyclerview/ImageAdapter.kt
+++ app/src/main/java/com/example/recyclerview/ImageAdapter.kt
@@ -1,35 +1,38 @@
 package com.example.recyclerview
 
+import android.provider.ContactsContract.CommonDataKinds.Im
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter (_images: Array<Int>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>(){
+class ImageAdapter (_imageItems: Array<ImageItem>) : RecyclerView.Adapter<ImageAdapter.ImageItemViewHolder>(){
 
-    private val images = _images
+    private val imageItems = _imageItems
 
-    class ImageViewHolder(_imageView: ImageView) : RecyclerView.ViewHolder(_imageView){
-        val imageView = _imageView
+    /*the inner keyword here means an instance of ImageItemViewHolder
+    will only ever exist inside an instance of ImageAdapter
+
+    this will allow members of ImageItemViewHolder to
+    access members of ImageAdapter
+    */
+    inner class ImageItemViewHolder(layout: View) : RecyclerView.ViewHolder(layout){
+        val imagineView = layout.findViewById<ImageView>(R.id.imageView)
+        val textView = layout.findViewById<TextView>(R.id.textView)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
-        /*
-        this returns an ImageViewHolder object without assigning to variable first
-
-
-         */
-        return ImageViewHolder(
-            ImageView(
-                parent.context
-            ).apply {
-                layoutParams = ViewGroup.LayoutParams(1000,1000)
-            }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageItemViewHolder {
+        return ImageItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
         )
     }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        holder.imageView.setImageResource(images[position])
+    override fun onBindViewHolder(holder: ImageItemViewHolder, position: Int) {
+        holder.imagineView.setImageResource(imageItems[position].id)
+        holder.textView.text = imageItems[position].name
     }
 
-    override fun getItemCount() = images.size
+    override fun getItemCount() = imageItems.size
 }
